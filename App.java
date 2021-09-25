482ab40b1aa930d15942eac805683f6421159c2d
manish661996 <manishpandey0526cs@gmail.com>
2021-09-25 14:06:46 +0530

    main class

---
+++ App.java
@@ -0,0 +1,38 @@
+package com.bridgelabz.Quaderetic;
+
+import org.apache.logging.log4j.Logger;
+
+
+
+import java.util.Scanner;
+import org.apache.logging.log4j.LogManager;
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	private static final Logger Log = LogManager.getLogger(App.class);
+
+    public static void main( String[] args )
+    {
+    	// this section of the code will generate log file and print log message on console
+		String message="Welcome ";
+		Log.debug(message + " will be printed on Debug");
+		Log.info(message + " Will be printed on info");
+		Log.warn(message + " will be printed on warn");
+		Log.error(message + " will be printed on error");
+		Log.fatal(message + " will be printed on fatal");
+		Log.info("Appending String: {}.", message);
+		System.out.println(message);
+		
+		Quadratic quad = new Quadratic();
+		quad.setA();
+		quad.setB();
+		quad.setC();
+		quad.setD();
+		quad.equation();
+		
+        
+    }
+}
